6f250e28a699fd46ef1df44c60573d627f1d3978
Mauricio <mauricio.lombano@gmail.com>
2023-08-07 15:53:34 -0600

    added method to create users

--- api-routes/usersData.js
+++ api-routes/usersData.js
@@ -2,14 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://root:Hyklv5gh@cluster0.dl9kn2d.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://maulom:rnreqcL5@logisticclcuster.8cqosl5.mongodb.net/";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 
 router.get("/", async (req, res) => {
     try{
-        const db = client.db("QuickpakMain");
+        const db = client.db("Quickpak_logistic");
         const clientsList = await db
-        .collection("clients")
+        .collection("users")
         .find({})
         .toArray()
         res.json({users:clientsList})
@@ -17,4 +17,26 @@ router.get("/", async (req, res) => {
         console.error("Error while get usersData: ", e)
     }
 })
+
+router.post("/", async (req,res) =>{
+    try{
+        if(!req?.body?.userName){
+            res.json({"message": "Cannot read username"})
+        }else if (!req?.body?.password) {
+            if(!req?.body?.password){
+                res.json({"message": "Cannot read password"})
+            }
+        } else {
+            const db = client.db("Quickpak_logistic");
+            const clientsList = await db
+            .collection("users")
+            .insertOne({"userName": req?.body?.userName, "password":req?.body?.password})
+            res.json({"message": "Added user"})
+        }
+    }catch(e){
+        console.error("Error while adding user: ", e)
+    
+    }})
+
 module.exports =router
+
