21cc47058e79f644246a0975633af82e384bc2ac
Mauricio <mauricio.lombano@gmail.com>
2024-08-05 19:27:04 -0700

    fixes

--- api-routes/getRates.js
+++ api-routes/getRates.js
@@ -74,7 +74,7 @@ router.post('/', async (req, res) => {
         const validServicesDHL = ["G", "N"]; // Your valid services for DHL
 
         const zonedhl = getzoneDHL.getZoneRequest(cpOrigin, cpDestino);
-        const pricesBasedOnClientData = controllerPrices.getPricesBasedOnSheet(dataResponseDHL, clientDataSheet, weightForCalcs, zonedhl, Number.parseFloat(ffTaxes?.FFTaxes?.aerial || 10.46), Number.parseFloat(ffTaxes?.FFTaxes?.land || 19.51), validServicesDHL);
+        const pricesBasedOnClientData = controllerPrices.getPricesBasedOnSheet(dataResponseDHL, clientDataSheet, weightForCalcs, zonedhl, Number.parseFloat(ffTaxes?.FFTaxes?.aerial || 11.18), Number.parseFloat(ffTaxes?.FFTaxes?.land || 19.71), validServicesDHL);
 
         return res.status(200).json({ status: "OK", messages: "ok", zone: zonedhl, data: pricesBasedOnClientData });
     } catch (error) {
--- api-routesV2/rates.js
+++ api-routesV2/rates.js
@@ -76,8 +76,8 @@ client.connect().then(() => {
     const usersCollection = db.collection("users");
     const userPricingCollection = db.collection("user_pricing")
 
-    const FFGroundTax = 19.51;
-    const FFAerialTax = 10.46
+    const FFGroundTax = 19.71;
+    const FFAerialTax = 11.18;
 
     router.post('/DHL', async (req, res) => {
         try {
